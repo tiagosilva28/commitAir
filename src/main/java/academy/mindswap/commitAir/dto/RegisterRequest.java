75d772c3735ac7f7883fb7759a2df4ddeca74f38
tiagosilva28 <tiagosilva28@live.com.pt>
2023-03-21 17:02:19 +0000

    rearrenge Services Classes with interfaces

--- src/main/java/academy/mindswap/commitAir/dto/RegisterRequest.java
+++ src/main/java/academy/mindswap/commitAir/dto/RegisterRequest.java
@@ -1,5 +1,6 @@
 package academy.mindswap.commitAir.dto;
 
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -11,9 +12,20 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class RegisterRequest {
 
+    @NotBlank(message = "Must have First Name")
     private String firstName;
+    @NotBlank(message = "Must have Last Name")
     private String lastName;
+    @Email(message = "Email must be valid")
+    @Pattern(regexp = "^[A-Za-z0-9+_.-]+@(.+)$", message = "Invalid email")
     private String email;
+
+    @Min(value = 8, message = "Password must be at least 8 characters long")
+    @NotBlank (message = "Must have password")
     private String password;
+
+    @Min(value = 8, message = "Password must be at least 8 characters long")
+    @NotBlank (message = "Must have password")
+    private String retypedPassword;
 
 }
