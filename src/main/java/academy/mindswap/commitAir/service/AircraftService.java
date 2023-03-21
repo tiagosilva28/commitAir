1141b2b1e04a0f08a54db8dc40d4dbd5ccd98da9
Milton <milton.caniceiro@gmail.com>
2023-03-21 17:39:57 +0000

    creation of service impl classes

--- src/main/java/academy/mindswap/commitAir/service/AircraftService.java
+++ src/main/java/academy/mindswap/commitAir/service/AircraftService.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class AircraftService {
+public interface AircraftService {
     AircraftDto createAircraft(RegisterRequest aircraft);
 }
 
