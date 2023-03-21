c4dc37596b0c0cbb985374bc85ac60a4d469cced
tiagosilva28 <tiagosilva28@live.com.pt>
2023-03-21 17:20:30 +0000

    rearrenge Services Classes with interfaces

--- src/main/java/academy/mindswap/commitAir/dto/UserDto.java
+++ src/main/java/academy/mindswap/commitAir/dto/UserDto.java
@@ -1,4 +1,28 @@
 package academy.mindswap.commitAir.dto;
 
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserDto {
+
+    @NotBlank(message = "Must have first name")
+    private String firstName;
+
+    @NotBlank(message = "Must have last name")
+    private String lastName;
+
+    @Email(message = "Email must be valid")
+    @Pattern(regexp = "^[A-Za-z0-9+_.-]+@(.+)$", message = "Invalid email")
+    private String email;
+
+
 }
