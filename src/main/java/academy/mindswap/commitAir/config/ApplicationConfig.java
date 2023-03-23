a8dcaa02b77e409eb5f5cf5d943469629fa11039
filipagoncalves <filipa.i.goncalves@gmail.com>
2023-03-23 10:12:18 +0000

    swagger dependencies

--- src/main/java/academy/mindswap/commitAir/config/ApplicationConfig.java
+++ src/main/java/academy/mindswap/commitAir/config/ApplicationConfig.java
@@ -1,4 +1,28 @@
 package academy.mindswap.commitAir.config;
 
+
+/*import org.springframework.context.annotation.Configuration;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import static springfox.documentation.builders.PathSelectors.regex;
+
+@EnableSwagger2
+@Configuration*/
 public class ApplicationConfig {
+
+    //@Bean
+    /*public Docket productApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.example.todo"))
+                .paths(regex("/todo.*"))
+                .build();
+    }*/
 }
