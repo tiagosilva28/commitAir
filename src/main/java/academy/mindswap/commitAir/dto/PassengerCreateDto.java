4af1d55c88d1ffcd99621180a4fd272fa0566427
Milton <milton.caniceiro@gmail.com>
2023-03-23 12:32:48 +0000

    passenger dto

---
+++ src/main/java/academy/mindswap/commitAir/dto/PassengerCreateDto.java
@@ -0,0 +1,26 @@
+package academy.mindswap.commitAir.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+import java.util.Date;
+
+@Getter
+@Setter
+@Builder
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class PassengerCreateDto {
+
+    @NotBlank(message = "Must have first name")
+    private String firstName;
+
+    @NotBlank(message = "Must have last name")
+    private String lastName;
+
+    @NotBlank (message = "Must have a Birth Date")
+    private Date dateOfBirth;
+
+    @NotBlank (message = "Must have a Nationality")
+    private String nationality;
+}
