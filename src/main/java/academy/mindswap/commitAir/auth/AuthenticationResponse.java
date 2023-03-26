f3c863405d93ca1e37ced9422581e45b7a18c490
tiagosilva28 <tiagosilva28@live.com.pt>
2023-03-26 15:55:47 +0100

    error comment

---
+++ src/main/java/academy/mindswap/commitAir/auth/AuthenticationResponse.java
@@ -0,0 +1,17 @@
+package academy.mindswap.commitAir.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class AuthenticationResponse {
+
+    private String token;
+
+}
---
+++ src/main/java/academy/mindswap/commitAir/model/Token.java
@@ -0,0 +1,34 @@
+package academy.mindswap.commitAir.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Token {
+
+    @Id
+    @GeneratedValue
+    public Integer id;
+
+    @Column(unique = true)
+    public String token;
+
+    @Enumerated(EnumType.STRING)
+    public TokenType tokenType = TokenType.BEARER;
+
+    public boolean revoked;
+
+    public boolean expired;
+
+    @ManyToOne
+    @JoinColumn(name = "users_id")
+    public User user;
+
+}
