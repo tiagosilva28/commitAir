3397be35592177203ae9c4f59db7f2f69754550d
filipagoncalves <filipa.i.goncalves@gmail.com>
2023-03-29 11:19:19 +0100

    Authorizations implementantion

--- src/main/java/academy/mindswap/commitAir/converter/SeatConverter.java
+++
@@ -1,7 +0,0 @@
-package academy.mindswap.commitAir.converter;
-
-import org.springframework.stereotype.Component;
-
-@Component
-public class SeatConverter {
-}
---
+++ src/main/java/academy/mindswap/commitAir/exception/UserNotMatch.java
@@ -0,0 +1,8 @@
+package academy.mindswap.commitAir.exception;
+
+public class UserNotMatch extends RuntimeException{
+
+    public UserNotMatch(String message) {
+        super(message);
+    }
+}
