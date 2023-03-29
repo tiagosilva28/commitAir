147aec08cf58e88dbf22e8558eafdeab9e8ea3c2
tiagosilva28 <tiagosilva28@live.com.pt>
2023-03-29 12:43:34 +0100

    Keys master class - Redis

--- src/main/java/academy/mindswap/commitAir/controller/AirportController.java
+++ src/main/java/academy/mindswap/commitAir/controller/AirportController.java
@@ -1,19 +1,12 @@
 package academy.mindswap.commitAir.controller;
 
 import academy.mindswap.commitAir.dto.AirportDto;
-import academy.mindswap.commitAir.dto.CityDto;
 import academy.mindswap.commitAir.service.AirportService;
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 
@@ -31,16 +24,7 @@ public class AirportController {
     public List<AirportDto> getAirports() throws JsonProcessingException {
         String uri = "https://airlabs.co/api/v9/airports?iata_code=CDG&api_key=YOUR-API-KEY";
 
-        RestTemplate restTemplate = new RestTemplate();
-        ObjectMapper objectMapper = new ObjectMapper();
 
-        ResponseEntity<AirportDto> responseEntity = restTemplate.exchange(uri, HttpMethod.GET, null, AirportDto.class);
-        JsonNode root = objectMapper.readTree(String.valueOf(responseEntity.getBody()));
-        JsonNode response = root.path("response");
-
-        List<AirportDto> airports =  objectMapper.readValue(response.toString(), new TypeReference<List<AirportDto>>() {});
-
-
-        return airports;
+        return null;
     }
 }
